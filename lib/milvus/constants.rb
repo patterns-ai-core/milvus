d1a06be57aeb39e9e6cb88098b7c71b168c0db93
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-07-01 12:39:55 -0400

    Update constants.rb

--- lib/milvus/constants.rb
+++ lib/milvus/constants.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Milvus
+  # https://milvus.io/api-reference/pymilvus/v2.4.x/MilvusClient/Collections/DataType.md
   DATA_TYPES = {
     "boolean" => 1,
     "int8" => 2,
@@ -11,7 +12,12 @@ module Milvus
     "double" => 11,
     "string" => 20,
     "varchar" => 21,
+    "array" => 22,
+    "json" => 23,
     "binary_vector" => 100,
-    "float_vector" => 101
+    "float_vector" => 101,
+    "float16_vector" => 102,
+    "bfloat16_vector" => 103,
+    "sparse_float_vector" => 104
   }.freeze
 end
